427b08c20c68f03cf4be9648b97993b38fc83ad1
Anastasiya Haiduk <anastasiagajduk24@gmail.com>
2026-05-13 20:29:49 +0200

    added edge saturation logic maintaining even degrees via 3-cycles

--- data/generator.py
+++ data/generator.py
@@ -12,3 +12,34 @@ def generate_hamiltonian_cycle(n):
         graph.add_edge(u, v)
         
     return graph
+
+def get_max_edges(n):
+    return n * (n - 1) // 2
+
+def saturate_graph(graph, target_saturation):
+    n = graph.num_vertices
+    max_edges = get_max_edges(n)
+    target_edges = int(max_edges * target_saturation)
+    
+    current_edges = n 
+    
+    while current_edges < target_edges:
+        v1, v2, v3 = random.sample(range(n), 3)
+
+        if (v2 not in graph.adj_list[v1] and 
+            v3 not in graph.adj_list[v2] and 
+            v1 not in graph.adj_list[v3]):
+            
+            graph.add_edge(v1, v2)
+            graph.add_edge(v2, v3)
+            graph.add_edge(v3, v1)
+            
+            current_edges += 3
+
+def generate_full_hamiltonian(n, saturation_percent):
+    if n <= 10:
+        raise ValueError("Ilość wierzchołków musi być większa niż 10")
+        
+    graph = generate_hamiltonian_cycle(n)
+    saturate_graph(graph, saturation_percent / 100.0)
+    return graph
